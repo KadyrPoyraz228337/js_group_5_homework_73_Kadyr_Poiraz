3416b3545458a38d1b091725d72abe0b8a99088e
Kadyr Poyraz <poyraz_2001@mail.ru>
2020-02-06 08:24:24 +0200

    Проект переделан под 75 д/з

--- server.js
+++ server.js
@@ -3,28 +3,20 @@ const cors = require('cors');
 const Vigenere = require('caesar-salad').Vigenere;
 
 const app = express();
+
+app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.post('/encode', (req, res) => {
     res.header('Content-Type', 'text/plain');
-    res.send(`"Home page"\n`)
+    res.send(Vigenere.Cipher(req.body.password).crypt(req.body.value))
 });
 
-app.get('/:value', (req, res) => {
+app.post('/decode', (req, res) => {
     res.header('Content-Type', 'text/plain');
-    res.send(`${req.params.value}\n`)
+    res.send(Vigenere.Decipher(req.body.password).crypt(req.body.value))
 });
 
-app.get('/encode/:value', (req, res) => {
-    res.header('Content-Type', 'text/plain');
-    res.send(Vigenere.Cipher('password').crypt(req.params.value))
-});
-
-app.get('/decode/:value', (req, res) => {
-    res.header('Content-Type', 'text/plain');
-    res.send(Vigenere.Decipher('password').crypt(req.params.value))
-});
-
-app.listen(8000, () => {
-    console.log('live in host 8000');
+app.listen(5000, () => {
+    console.log('live in host 5000');
 });
